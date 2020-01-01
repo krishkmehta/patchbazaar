673cd0932af2175acdb26a1b846160ab8fa07226
Sarab Sodhi <sarabjeet@kinexmedia.com>
2020-01-01 17:13:46 +0530

    Size Chart Page

--- wp-content/themes/ecommerce-gem/sss/templates/size_charts.php
+++ wp-content/themes/ecommerce-gem/sss/templates/size_charts.php
@@ -9,7 +9,7 @@
 $qty_array = array(10, 20, 50, 100, 200, 300, 500, 1000, 2000, 5000);
 $args = array(
     'post_type' => 'product',
-    'order'=>'asc',
+    'order' => 'asc',
     'posts_per_page' => -1,
     'tax_query' => array(
         array(
@@ -26,81 +26,122 @@ $args = array(
         )
     )
 );
-$posts = get_posts($args);
+$custom_query = new WP_Query($args);
 
-if ($posts) {
-    foreach ($posts as $post) {
-        echo "<h4 class='table-title'>".get_the_title()."</h4>";
-        $size_charts = get_posts(array(
-            'post_type' => 'size_chart',
-            'posts_per_page' => -1,
-            'meta_query' => array(
-                array(
-                    'key' => 'size_price_products', // name of custom field
-                    'value' => '"' . get_the_ID() . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
-                    'compare' => 'LIKE'
-                )
-            )
-        ));
+if ($custom_query->have_posts()) {
+    ?>
 
-
-
-        global $post;
-        $tables = array();
-        if ($size_charts) {
-            foreach ($size_charts as $post) {
-                setup_postdata($post);
-                $size_group = get_field('size_price');
-                $size = $size_group['size'];
-                foreach ($size_group['price_repeater'] as $prices) {
-
-                    $tables[$size][$prices['qty']] = $prices['price'];
-                }
-
-            }
-            wp_reset_postdata();
-        }
-        if (count($tables) > 0) {
-
-            ?>
-            <div class="table-responsive">
-            <table class="table table-condensed custom-tables">
-                <thead>
-                <tr>
-                    <td>Size</td>
-                    <?php
-                    foreach ($qty_array as $qty) {
-                        echo "<td>{$qty}</td>";
-                    }
-                    ?>
-                </tr>
-                </thead>
-
-                <tbody>
+    <div id="horizontalTab">
+        <?php
+        $content_array = array();
+        $index = 1;
+        ?>
+        <ul>
+            <?php while ($custom_query->have_posts()) :$custom_query->the_post(); ?>
 
                 <?php
+                $title = get_the_title();
+                $size_charts = get_posts(array(
+                    'post_type' => 'size_chart',
+                    'posts_per_page' => -1,
+                    'meta_query' => array(
+                        array(
+                            'key' => 'size_price_products', // name of custom field
+                            'value' => '"' . get_the_ID() . '"', // matches exactly "123", not just 123. This prevents a
+                            'compare' => 'LIKE'
+                        )
+                    )
+                ));
 
-                foreach ($tables as $key => $table) {
-                    echo "<tr>";
-                    echo "<td>{$key}\"</td>";
-                    foreach ($qty_array as $qty) {
+                $tables = array();
 
-                        echo "<td>" . (isset($table[$qty]) ? $table[$qty] : '-') . "</td>";
+                if (count($size_charts) > 0) {
+                    global $post;
+                    foreach ($size_charts as $post) {
+
+                        setup_postdata($post);
+                        $size_group = get_field('size_price');
+                        $size = $size_group['size'];
+                        foreach ($size_group['price_repeater'] as $prices) {
+                            $tables[$size][$prices['qty']] = $prices['price'];
+                        }
+
                     }
-
-                    echo "</tr>";
+                    wp_reset_postdata();
                 }
 
                 ?>
-                </tr>
-                </tbody>
-            </table>
+                <?php if (count($tables) > 0): ?>
+                    <li>
+                        <a href="#tab-<?php echo $index; ?>">
+                            <h4 class='table-title'>
+
+                                <?php echo $title; ?>
+                                <span class="fa">
+                            </h4>
+                        </a>
+                    </li>
+                    <?php
+                    $content_array[$index] = $tables;
+                    ?>
+                    <?php $index++; ?>
+
+                <?php endif; ?>
+
+            <?php endwhile; ?>
+        </ul>
+
+        <?php foreach ($content_array as $key => $tables): ?>
+            <div id="tab-<?php echo $key ?>" class="table-responsive">
+                <table class="table table-condensed custom-tables">
+                    <thead>
+                    <tr>
+                        <td>Size</td>
+                        <?php
+                        foreach ($qty_array as $qty) {
+                            echo "<td>{$qty}</td>";
+                        }
+                        ?>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+
+                    <?php
+
+                    foreach ($tables as $key => $table) {
+                        echo "<tr>";
+                        echo "<td>{$key}\"</td>";
+                        foreach ($qty_array as $qty) {
+
+                            echo "<td>" . (isset($table[$qty]) ? $table[$qty] : '-') . "</td>";
+                        }
+
+                        echo "</tr>";
+                    }
+
+                    ?>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
-            <?php
-        }
 
+        <?php endforeach; ?>
+    </div>
+    <?php wp_reset_query() ?>
+    <script src="<?php echo get_stylesheet_directory_uri() ?>/assets/third-party/accordionJs/js/jquery.responsiveTabs.js"></script>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/third-party/accordionJs/css/responsive-tabs.css">
 
-    }
-}
+    <script type="text/javascript">
+        jQuery(document).ready(function () {
+            var $tabs = jQuery('#horizontalTab');
+            $tabs.responsiveTabs({
+                rotate: false,
+                startCollapsed: 'accordion',
+                collapsible: 'accordion',
+                setHash: true,
 
-
+            });
+        });
+    </script>
+<?php } ?>
